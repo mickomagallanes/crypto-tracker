6d4f81e25ddc844f488617d6f2713d2e13a96c73
Micko Magallanes <mickomagallanes098@gmail.com>
2024-06-28 17:18:54 +0800

    quick fix test prod

--- app/[symbol]/(skeleton)/header-skeleton.tsx
+++ app/[symbol]/(skeleton)/header-skeleton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeaderSkeleton() {
   return (
-    <div className="flex animate-pulse flex-col gap-2">
+    <div className="flex w-full animate-pulse flex-col gap-2">
       <div className="flex flex-row items-center gap-1">
         <div className="h-4 w-1/4 rounded-md bg-slate-200"></div>
       </div>
