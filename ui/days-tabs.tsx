fbde1149e086b162c40f2cc0a2dd9920124d2060
Micko Magallanes <mickomagallanes098@gmail.com>
2024-07-20 02:21:37 +0800

    modal in perf

---
+++ ui/days-tabs.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import React from "react";
+
+export default function DaysTabs() {
+  const searchParams = useSearchParams();
+  const currentDays = searchParams.get("days");
+
+  const pathname = usePathname();
+
+  const isActive = (dayValue: string) => {
+    if (
+      currentDays === dayValue ||
+      (currentDays === null && dayValue === "1") // default highlight by 1
+    ) {
+      return `bg-gray-900 text-pink-400 px-3 md:px-4`;
+    } else {
+      return `px-2 md:px-3`;
+    }
+  };
+
+  return (
+    <div className="flex w-max flex-row flex-wrap gap-2 rounded-lg bg-gray-800 p-1 text-center text-sm md:text-base">
+      <Link
+        className={cn("m-auto rounded-md py-1", isActive("1"))}
+        href={`${pathname}?days=1`}
+      >
+        1D
+      </Link>
+      <Link
+        className={cn("m-auto rounded-md py-1", isActive("7"))}
+        href={`${pathname}?days=7`}
+      >
+        7D
+      </Link>
+      <Link
+        className={cn("m-auto rounded-md py-1", isActive("30"))}
+        href={`${pathname}?days=30`}
+      >
+        1M
+      </Link>
+      <Link
+        className={cn("m-auto rounded-md py-1", isActive("60"))}
+        href={`${pathname}?days=60`}
+      >
+        2M
+      </Link>
+      <Link
+        className={cn("m-auto rounded-md py-1", isActive("365"))}
+        href={`${pathname}?days=365`}
+      >
+        1Y
+      </Link>
+    </div>
+  );
+}
---
+++ ui/modal.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { X } from "lucide-react";
+import React, { ReactNode, useEffect } from "react";
+
+export default function Modal({
+  children,
+  header = "",
+  isOpen = false,
+  onClose = () => {},
+}: {
+  children: ReactNode;
+  header?: ReactNode | "";
+  isOpen?: boolean;
+  onClose?: () => void;
+}) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  return (
+    <>
+      <div
+        id="default-modal"
+        aria-hidden="true"
+        className={cn(
+          `fixed inset-0 z-50 max-h-full 
+           w-full items-center justify-center overflow-y-auto overflow-x-hidden 
+           bg-gray-900/70 
+           transition-all sm:p-6 lg:py-12`,
+          !isOpen && "hidden",
+        )}
+      >
+        <div className="mx-auto rounded-md bg-gray-800 px-2 shadow-lg sm:w-5/6 md:w-3/4 md:px-4 lg:w-7/12 xl:w-1/2">
+          <div className="flex w-full justify-between pt-4">
+            <h2 className="m-1 py-1 pl-3 text-xl font-bold">{header}</h2>
+            <span
+              onClick={onClose}
+              className="m-1 flex cursor-pointer items-center rounded-lg px-1 hover:bg-gray-900 hover:text-purple-500"
+            >
+              <X size={21} />
+            </span>
+          </div>
+          <div className="p-4">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
